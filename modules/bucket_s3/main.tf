aadcc0a4b31366a4e07144b6f89902bcdde0016c
Salvadortarrio <salvador.tarrio@campusdual.com>
2024-12-13 20:51:19 +0100

    muchos cambios

---
+++ modules/bucket_s3/main.tf
@@ -0,0 +1,11 @@
+# Crear un bucket S3
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name  # Usamos el nombre del bucket desde una variable
+  acl    = var.acl          # Política de acceso, definida en la variable
+  # Habilitar la versión de los objetos del bucket (opcional)
+  versioning {
+    enabled = false
+  }
+}
+
+
---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+# Declaración de variables
+
+variable "aws_region" {
+  description = "La región de AWS donde se desplegarán los recursos"
+  type        = string
+  #default     = "eu-west-3"  # Valor por defecto para la región (París)
+}
+
+variable "ami_id" {
+  description = "ID de la AMI de Red Hat en la región especificada"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Tipo de la instancia EC2"
+  type        = string
+  #default     = "c5.large"  # Valor por defecto para el tipo de instancia
+}
+
+variable "bucket_name" {
+  description = "Nombre del bucket S3"
+  type        = string
+}
+
+variable "acl" {
+  description = "Política de acceso al bucket"
+  type        = string
+  default     = "private"  # Valor por defecto para la política de acceso
+}
+
